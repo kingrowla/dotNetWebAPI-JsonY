354102f6b8cd3855200081410d65dca3a8635330
ansont <ansont@collectorsolutions.com>
2016-10-21 16:58:37 -0500

    add models controllers

---
+++ dotNetWebAPI/Controllers/CalculateFeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace dotNetWebAPI.Controllers
+{
+
+    [Authorize]
+    [RoutePrefix("api/CalculateFee")]
+    public class CalculateFeeController : ApiController
+    {
+        // GET api/values
+        public string Get()
+        {
+            return "Please use Post method to get a fee.";
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "Please use Post method to get a fee.";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
---
+++ dotNetWebAPI/Models/CalculateFeeModels.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace dotNetWebAPI.Models
+{
+    public class CalculateFeeModels
+    {
+        public class LINEITEM
+        {
+            [DisplayName("Client Payment Type Identifier")]
+            [JsonProperty(PropertyName = "CUSTOMERPAYMENTTYPEID")]
+            public string CUSTOMERPAYMENTTYPEID { get; set; }
+            [DisplayName("Item Amount")]
+            [JsonProperty(PropertyName = "ITEMAMOUNT")]
+            public string ITEMAMOUNT { get; set; }
+        }
+
+        public class FEE
+        {
+            [DisplayName("Client Key")]
+            [JsonProperty(PropertyName = "CLIENTKEY")]
+            public string CLIENTKEY { get; set; }
+            [DisplayName("Payment Method")]
+            [JsonProperty(PropertyName = "PAYMENTMETHOD")]
+            public string PAYMENTMETHOD { get; set; }
+            [DisplayName("Card Type")]
+            [JsonProperty(PropertyName = "CARDTYPE")]
+            public string CARDTYPE { get; set; }
+            [DisplayName("Collection Mode")]
+            [JsonProperty(PropertyName = "COLLECTIONMODE")]
+            public string COLLECTIONMODE { get; set; }
+            [DisplayName("Amount")]
+            [JsonProperty(PropertyName = "AMOUNT")]
+            public string AMOUNT { get; set; }
+            [DisplayName("Line Item")]
+            [JsonProperty(PropertyName = "LINEITEM")]
+            public LINEITEM LINEITEM { get; set; }
+        }
+    }
+}
