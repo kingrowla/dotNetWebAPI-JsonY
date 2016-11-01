163d9ee8a2ff038efbb09b57518ce2c6faf0468a
ansont <ansont@collectorsolutions.com>
2016-11-01 09:15:17 -0500

    help file update

--- dotNetWebAPI/App_Start/BundleConfig.cs
+++ dotNetWebAPI/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace dotNetWebAPI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
